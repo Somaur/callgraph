ac2e43491f0c9aeee16494049b16f69db1d0feb1
Yunus Emre Gül <yunemregul@gmail.com>
2023-06-10 17:52:10 +0300

    Remove disposing for now

--- src/main/java/callgraph/callgraph/CallGraphToolWindowFactory.java
+++ src/main/java/callgraph/callgraph/CallGraphToolWindowFactory.java
@@ -11,7 +11,6 @@ import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
 import com.intellij.psi.PsiDocumentManager;
